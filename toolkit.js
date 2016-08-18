de7578fe4fd3ec270bd0c8890c4aeb4f97c4bd3a
Dustin John Pfister <dustin.pfister@gmail.com>
2016-08-17 23:00:32 -0400

    pubApi var

--- toolkit.js
+++ toolkit.js
@@ -7,8 +7,97 @@
 // Swarm Sim Tools
 var ssTools = (function () {
 
+    var pubAPI = {
+
+        // ssTools version
+        version : '1.1.6',
+
+        // current status of production of bug of interest (drones, queens, ... , Neural Clusters, ect)
+        bpr : 22.5579 * Math.pow(10, 30), // bug production rate of bug of interest
+        bpl : 134 * Math.pow(10, 6), // bugs per larva
+
+        // energy tab status
+        m : 19, // swarm warp minutes
+        s : 0, // swarm warp seconds
+        larvaPerC : 422.456 * Math.pow(10, 24), // larva per cone
+
+        /*    swarmwarp:
+         *
+         *    return total production per 1000e if swarmwarp
+         *    where bpr is the bug production rate per second
+         *    and m, and s are minutes and seconds.
+         */
+        warp : function () {
+
+            return this.bpr * (60 * this.m + this.s) / 2;
+
+        },
+
+        /*    cloning:
+         *
+         *    return total production per 1000e if cloning
+         *    where bpl is the bug production per larva, and larvaPerC is the
+         *    larva production per clone
+         */
+        clone : function () {
+
+            return this.bpl * this.larvaPerC / 12;
+
+        },
+
+        // do i clone or warp?
+        what : function () {
+
+            if (this.clone() > this.warp()) {
+
+                return 'clone';
+
+            } else {
+
+                return 'warp'
+
+            }
+
+        },
+
+        meat : function () {
+
+            updateMeat();
+
+        },
+
+        toTab : function (index) {
+
+            toTab(index);
+            console.log(document.getElementsByClassName('unit-table'));
+
+        },
+
+        // grab values from dom
+        grab : function () {
+
+            //console.log(document.body);
+
+            //    var appElement = document.querySelector('[ng-app=swarmApp]');
+            //appScope = angular.element(appElement).scope();
+
+            // class 'tab-icon-meat'
+
+
+            //  console.log(document.querySelector('[ng-app=swarmApp]'));
+            //  console.log(document.getElementsByClassName('tab-icon-meat'));
+            //  console.log(document.getElementsByClassName('ng-binding'));
+
+            // get the unit table
+            //  console.log(document.getElementsByClassName('unit-table'));
+            //  console.log(document.getElementsByTagName('tbody'));
+
+        }
+
+    },
+
     // tab elements
-    var tabs = [],
+    tabs = [],
 
     // to tab helper ( index 0 ==  meat tab, 1 == larva tab, ...)
     toTab = function (index) {
@@ -147,95 +236,8 @@ var ssTools = (function () {
     }
         ());
 
-    // public api
-    return {
-
-        // ssTools version
-        version : '1.1.5',
-
-        // current status of production of bug of interest (drones, queens, ... , Neural Clusters, ect)
-        bpr : 22.5579 * Math.pow(10, 30), // bug production rate of bug of interest
-        bpl : 134 * Math.pow(10, 6), // bugs per larva
-
-        // energy tab status
-        m : 19, // swarm warp minutes
-        s : 0, // swarm warp seconds
-        larvaPerC : 422.456 * Math.pow(10, 24), // larva per cone
-
-        /*    swarmwarp:
-         *
-         *    return total production per 1000e if swarmwarp
-         *    where bpr is the bug production rate per second
-         *    and m, and s are minutes and seconds.
-         */
-        warp : function () {
-
-            return this.bpr * (60 * this.m + this.s) / 2;
-
-        },
-
-        /*    cloning:
-         *
-         *    return total production per 1000e if cloning
-         *    where bpl is the bug production per larva, and larvaPerC is the
-         *    larva production per clone
-         */
-        clone : function () {
-
-            return this.bpl * this.larvaPerC / 12;
-
-        },
-
-        // do i clone or warp?
-        what : function () {
-
-            if (this.clone() > this.warp()) {
-
-                return 'clone';
-
-            } else {
-
-                return 'warp'
-
-            }
-
-        },
-
-        meat : function () {
-
-            updateMeat();
-
-        },
-
-        toTab : function (index) {
-
-            toTab(index);
-            console.log(document.getElementsByClassName('unit-table'));
-
-        },
-
-        // grab values from dom
-        grab : function () {
-
-            //console.log(document.body);
-
-            //    var appElement = document.querySelector('[ng-app=swarmApp]');
-            //appScope = angular.element(appElement).scope();
-
-            // class 'tab-icon-meat'
-
-
-            //  console.log(document.querySelector('[ng-app=swarmApp]'));
-            //  console.log(document.getElementsByClassName('tab-icon-meat'));
-            //  console.log(document.getElementsByClassName('ng-binding'));
-
-            // get the unit table
-            //  console.log(document.getElementsByClassName('unit-table'));
-            //  console.log(document.getElementsByTagName('tbody'));
-
-        }
-
-    };
+    // return public api
+    return pubAPI;
 
 }
     ());
